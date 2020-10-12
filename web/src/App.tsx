c4073f0bc75876a596bc76f6136aae75d08a4e97
HenriqueHS19 <henriquesantos.hs34@gmail.com>
2020-10-12 19:33:09 -0300

    beginning of the frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import './styles/global.css';
+
+const App: React.FC = function () {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
---
+++ web/src/pages/OrphanageMap/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMarkerIcon from '../../images/map-marker.svg';
+
+import 'leaflet/dist/leaflet.css';
+
+import './styles.css';
+
+const OrphanageMap: React.FC = function () {
+    return (
+        <div id="page-map">
+
+            <div className="side-bar">
+                <div className="info">
+                    <img src={mapMarkerIcon} alt="Happy" />
+
+                    <h2> Escolha um orfanato no mapa </h2>
+                    <p> Muitas crianças estão esperando a sua visita :)</p>
+                </div>
+
+                <div className="location">
+                    <strong> São Paulo </strong>
+                    <p> São Paulo </p>
+                </div>
+            </div>
+
+            <Map
+                center={[-23.7646544, -46.6802013]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+
+            <Link to="" className="btnOrphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+        </div>
+    );
+}
+
+export default OrphanageMap;
